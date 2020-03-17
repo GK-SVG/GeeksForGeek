af804be7833d8fe7a3950800aaa919f693627153
gk-svg <gk32239@gmail.com>
2020-03-17 12:45:45 +0530

    practice problem

---
+++ medium/array/gothamSave.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t ;
+    cin>>t;
+    while (t--)
+    {
+        int n,add=0;
+        cin>>n;
+        int arr[n];
+        for(int i=0 ;i<n;i++)
+        cin>>arr[i];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i+1; j < n; j++)
+            {
+                if(arr[i]<arr[j])
+                {
+                    add+=arr[j];
+                    break;
+                }
+            }
+            
+        }
+       cout<<add<<endl; 
+    }
+}
